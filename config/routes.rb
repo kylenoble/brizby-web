6c4f8be399b8ab6fdad63331049edf5e6b3a57ea
kylenoble <kyle.nobl@gmail.com>
2014-12-08 17:51:12 -0500

    Changed Url Scheme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-	namespace :api do
+	namespace :api, path: '/', constraints: { subdomain: 'api' } do
 		namespace :v1 do
 			devise_for :users
 		end
 	end
+
+	root 'home#index'
 end
