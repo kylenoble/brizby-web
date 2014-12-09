092a8b24075197f059b2c43e35c35ce60624d875
kylenoble <kyle.nobl@gmail.com>
2014-12-08 23:32:36 -0500

    updated signup requirement

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -13,7 +13,7 @@ module V1
     private
 
     def user_params
-      params.require(:user).permit(:email, :password, :username)
+      params.require(:email, :password, :username)
     end
   end
 end
