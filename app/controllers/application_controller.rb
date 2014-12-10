d024cba65675f20a9ea2c20bc8c01731e9ed13b7
kylenoble <kyle.nobl@gmail.com>
2014-12-10 01:47:08 -0500

    Updated sign ins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
 end
